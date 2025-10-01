d1d0a9841c67596ec7c9bd642ff7b2c8420107ad
hugo.gilbert <gilbert.hugo1@gmail.com>
2025-10-01 14:27:38 +0200

    Mise à jour tilemap.g.jres, tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -10,6 +10,35 @@ namespace myTiles {
     export const tile4 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "map":
+            case "map1":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000001000000000000000000000000000002020000000000000000000000000000000100000000000000000000000000000004040500000000000000000000000000010000000000000000000000000000000200000000000000000000000000050203000000000000000000000000000000020000000000000000000000000003030102020100000000000000000000020000000000000000000000000000000400000000000000000000000000000004050000000000000000000000000000030000000000000000000000000000000200000000000000000000000000000001000000000000000000`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5], TileScale.Sixteen);
+        }
+        return null;
+    })
 
     helpers._registerFactory("tile", function(name: string) {
         switch(helpers.stringTrim(name)) {
@@ -23,6 +52,9 @@ namespace myTiles {
             case "tile4":return tile4;
             case "trou noir":
             case "tile5":return tile5;
+            case "transparency16":return transparency16;
+            case "arrivée":
+            case "tile6":return tile6;
         }
         return null;
     })

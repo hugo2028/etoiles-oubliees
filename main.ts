10c9d11442381fcb2fd83b89ad612ddc7c281961
hugo.gilbert <gilbert.hugo1@gmail.com>
2025-10-01 15:59:54 +0200

    Mise à jour main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -3,16 +3,16 @@ namespace SpriteKind {
 }
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile`, function (sprite, location) {
     console.log("Donne moi la spécialité d'Occitanie")
-    if (game.askForString("") == "cassoulet") {
-        tiles.placeOnTile(mySprite, tiles.getTileLocation(10, 12))
+    if (game.askForString("Donne moi la spécialité d'Occitanie") == "cassoulet") {
+        tiles.placeOnTile(mySprite, tiles.getTileLocation(7, 10))
     } else {
         tiles.placeOnTile(mySprite, tiles.getTileLocation(6, 15))
     }
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`rouge`, function (sprite, location) {
     console.log("Quel est la capitale de la France")
-    if (game.askForString("") == "Paris") {
-        tiles.placeOnTile(mySprite, tiles.getTileLocation(6, 13))
+    if (game.askForString("Quel est la capitale de la France") == "paris") {
+        tiles.placeOnTile(mySprite, tiles.getTileLocation(12, 5))
     } else {
         tiles.placeOnTile(mySprite, tiles.getTileLocation(6, 15))
     }
@@ -22,17 +22,20 @@ scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardSpike, function (sp
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`trou noir`, function (sprite, location) {
     tiles.placeOnTile(mySprite, tiles.getTileLocation(6, 15))
+    game.splash("Tu es tombé dans un trou noir")
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`vert`, function (sprite, location) {
     console.log("Dans quel pays les émeutes de Stonewall (1969) ont-elles marqué un tournant pour les droits LGBTQ+ ?")
-    if (game.askForString("") == "Etats-Unis") {
-        tiles.placeOnTile(mySprite, tiles.getTileLocation(6, 13))
+    if (game.askForString("Dans quel pays les émeutes de Stonewall (1969) ont-elles marqué un tournant pour les droits LGBTQ+ ?") == "etats-unis") {
+        tiles.placeOnTile(mySprite, tiles.getTileLocation(5, 7))
     } else {
         tiles.placeOnTile(mySprite, tiles.getTileLocation(6, 15))
     }
 })
 let mySprite: Sprite = null
+music.play(music.stringPlayable("A G A F A G A E ", 120), music.PlaybackMode.UntilDone)
 tiles.setWallAt(tiles.getTileLocation(0, 0), true)
+game.splash("Avance sur le plateau et répond aux questions")
 mySprite = sprites.create(assets.image`pion`, SpriteKind.Player)
 mySprite.startEffect(effects.spray)
 tiles.setCurrentTilemap(tilemap`map`)
